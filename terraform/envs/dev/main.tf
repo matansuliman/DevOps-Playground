b699c10015d220b8a1917b5bc3d0b0eb35227653
matansuliman <matan.2001@gmail.com>
2025-12-14 14:52:51 +0200

    add initial Terraform configuration for VPC, RDS, EFS, ALB, and ASG modules with outputs

---
+++ terraform/envs/dev/main.tf
@@ -0,0 +1,90 @@
+locals {
+  azs                 = ["eu-north-1a", "eu-north-1b"]
+  public_subnet_cidrs = ["10.0.0.0/20", "10.0.16.0/20"]
+
+  az_to_subnet = zipmap(local.azs, module.vpc.public_subnet_ids)
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  name       = "legi-bit"
+  cidr_block = "10.0.0.0/16"
+
+  availability_zones  = local.azs
+  public_subnet_cidrs = local.public_subnet_cidrs
+}
+
+# ---------- RDS MySQL ----------
+module "rds" {
+  source = "../../modules/rds-mysql"
+
+  name   = "legi-bit"
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.public_subnet_ids
+
+  allowed_sg_id = module.asg.instance_sg_id
+
+  db_name     = var.rds_db_name
+  db_username = var.rds_db_username
+  db_password = var.rds_db_password
+
+  instance_class    = var.rds_instance_class
+  allocated_storage = var.rds_allocated_storage
+}
+
+# ---------- EFS for WP content ----------
+module "efs" {
+  source = "../../modules/efs-wp-content"
+
+  name   = "legi-bit"
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.public_subnet_ids
+
+  allowed_sg_id = module.asg.instance_sg_id
+
+  performance_mode = var.efs_performance_mode
+  throughput_mode  = var.efs_throughput_mode
+  encrypted        = var.efs_encrypted
+}
+
+# ---------- ALB ----------
+module "alb" {
+  source = "../../modules/alb"
+
+  name       = "legi-bit"
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnet_ids
+
+  health_check_path = var.alb_health_check_path
+}
+
+# ---------- ASG WordPress ----------
+module "asg" {
+  source = "../../modules/asg-wordpress"
+
+  name   = "legi-bit"
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.public_subnet_ids
+
+  alb_sg_id        = module.alb.alb_security_group_id
+  target_group_arn = module.alb.target_group_arn
+
+  instance_type = var.asg_instance_type
+
+  min_size         = 1
+  desired_capacity = 1
+  max_size         = 2
+
+  wp_db_host     = module.rds.endpoint
+  wp_db_name     = var.rds_db_name
+  wp_db_user     = var.rds_db_username
+  wp_db_password = var.rds_db_password
+
+  efs_dns_name = module.efs.dns_name
+}
+
+
---
+++ terraform/envs/dev/outputs.tf
@@ -0,0 +1,61 @@
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "VPC ID"
+}
+
+output "public_subnet_ids" {
+  value       = module.vpc.public_subnet_ids
+  description = "Public subnet IDs"
+}
+
+output "availability_zones" {
+  value       = local.azs
+  description = "AZs in use"
+}
+
+/*
+output "ec2_instance_ids_by_az" {
+  value       = { for az, m in module.ec2 : az => m.instance_id }
+  description = "Instance IDs per AZ"
+}
+
+output "ec2_public_ips_by_az" {
+  value       = { for az, m in module.ec2 : az => m.public_ip }
+  description = "Public IPv4 per AZ"
+}
+
+output "ec2_public_dns_by_az" {
+  value       = { for az, m in module.ec2 : az => m.public_dns }
+  description = "Public DNS per AZ"
+}
+*/
+
+output "rds_endpoint" {
+  value = module.rds.endpoint
+}
+
+output "rds_port" {
+  value = module.rds.port
+}
+
+output "efs_id" {
+  value = module.efs.file_system_id
+}
+
+output "efs_dns_name" {
+  value = module.efs.dns_name
+}
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "alb_security_group_id" {
+  value = module.alb.alb_security_group_id
+}
+
+output "target_group_arn" {
+  value = module.alb.target_group_arn
+}
+
+
